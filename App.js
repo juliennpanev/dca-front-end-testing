bb5bf167693ac5d682ea8076d4361303238b194e
juliennpanev <83950947+juliennpanev@users.noreply.github.com>
2023-08-14 12:23:53 +0300

    withdraw amount for TCI round problem fixed

--- App.js
+++ App.js
@@ -1,39 +1,43 @@
-import switchPage from "./utils/pageSwitch.js"
-import showInfoWindow from "./utils/info.js"
-import pullPrice from "./utils/plutoPrice.js"
-import connect from './utils/connect.js'
-import getStakedCapital from "./utils/calcBaseCapital.js"
-import getDcaHolders from "./utils/getDcaHolders.js"
-import depositWindow from "./utils/depositWindow.js"
-import invoke from "./utils/invoke.js"
-import withdraw from './utils/withdraw.js'
-import setBaseAssetBalance from "./utils/setPlutoBalance.js"
-import setMaxDeposit from "./utils/setPlutoBalance.js"
+// import switchPage from "./utils/pageSwitch.js"
+// import showInfoWindow from "./utils/info.js"
+// import pullPrice from "./utils/plutoPrice.js"
+// import connect from './utils/connect.js'
+// import getStakedCapital from "./utils/calcBaseCapital.js"
+// import getDcaHolders from "./utils/getDcaHolders.js"
+// import depositWindow from "./utils/depositWindow.js"
+// import invoke from "./utils/invoke.js"
+// import withdraw from './utils/withdraw.js'
+// import setBaseAssetBalance from "./utils/setPlutoBalance.js"
+// import setMaxDeposit from "./utils/setPlutoBalance.js"
 
-import manageTCI from "./tci/tciHandler.js"
-import manageBTCSection from "./plutoBTC/manageViews.js"
+// import manageTCI from "./tci/tciHandler.js"
+// import manageBTCSection from "./plutoBTC/manageViews.js"
 
 
 
-const genesisTreasury = '3PAFi86jNXNmgc4Fx4RnV6aJLXcW4zn99LJ';
-const roundCapitalElement = document.getElementById('upcoming-round-total');
-const depositInput = document.getElementById('deposit-price')
+// const genesisTreasury = '3PAFi86jNXNmgc4Fx4RnV6aJLXcW4zn99LJ';
+// const roundCapitalElement = document.getElementById('upcoming-round-total');
+// const depositInput = document.getElementById('deposit-price')
 
-connect.checkIfConnected();
-connect.setPlutoBalance();
-invoke();
-setMaxDeposit(depositInput);
-connect.init();
-switchPage();
-showInfoWindow();
-pullPrice.calcUsdnValue();
-getStakedCapital(genesisTreasury, roundCapitalElement);
-getDcaHolders.getParticipants();
-depositWindow.calculateDepositWindow();
-setBaseAssetBalance();
-withdraw.loadWithdrawInfo();
-withdraw.activateWithdrawBtn();
-withdraw.setMaxWithdraw();
+// connect.checkIfConnected();
+// connect.setPlutoBalance();
+// invoke();
+// setMaxDeposit(depositInput);
+// connect.init();
+// switchPage();
+// showInfoWindow();
+// pullPrice.calcUsdnValue();
+// getStakedCapital(genesisTreasury, roundCapitalElement);
+// getDcaHolders.getParticipants();
+// depositWindow.calculateDepositWindow();
+// setBaseAssetBalance();
+// withdraw.loadWithdrawInfo();
+// withdraw.activateWithdrawBtn();
+// withdraw.setMaxWithdraw();
 
-manageBTCSection();
-manageTCI();
+// manageBTCSection();
+// manageTCI();
+
+import run from "./run.js"
+
+run();
